7d7ef750fcb2c6fc471e9d98df47b493a1a35816
Mikle Scott <mikhail-bulatov@bk.ru>
2021-04-02 20:02:05 +0500

    task2comm

---
+++ F.h
@@ -0,0 +1,11 @@
+
+
+#include "Value.h"
+
+class F : public Value
+{
+public:
+	F(double val) :Value(val) {};
+};
+
+
---
+++ S.h
@@ -0,0 +1,12 @@
+
+
+#include "Value.h"
+
+class S : public Value
+{
+public:
+	S(double val) :Value(val) {};
+};
+
+
+
